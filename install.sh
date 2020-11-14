125af85fcadaf0b634e4c1a5af57714d2e8fa6f1
root <root@localhost.localdomain>
2020-11-15 03:55:27 +0400

    poprawiono install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 rm -Rf /var/wwww/html/prestashop;
-cp ./prestashop /var/html/prestashop;
+cp ./prestashop /var/www/html/prestashop;
 find /var/www/html/prestashop -exec chown apache:apache {} \;
-chown /var/www/html/prestashop apache:apache;
+chown apache:apache /var/www/html/prestashop;
 
 # ausearch -c 'sendmail' --raw | audit2allow -M my-sendmail
 # semodule -i my-sendmail.pp
