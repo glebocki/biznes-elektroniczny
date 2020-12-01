a9e80a01f68a9aa815c3c881fcc162851080add1
root <root@localhost.localdomain>
2020-12-01 17:54:20 +0400

    Zmodyfikowano spidera - szuka zdjec, wiecej szczegolow i przeglada kolejne strony

--- spider.py
+++ spider.py
@@ -1,3 +1,5 @@
+#skrypt robiony na statku, nastepuje przekierowanie na strone en. Nalezy zamienic "color:"" na "kolor:"" oraz "ON EQUEST"
+
 import scrapy
 
 
@@ -12,6 +14,10 @@ class SpiderSpider(scrapy.Spider):
         for product in all_products:
             product_url = product.xpath('.//div/div/a/@href').extract_first()
             yield scrapy.Request(product_url, callback=self.parse_product)
+        
+        next_page_url = response.xpath('//a[@rel="next"]/@href').extract_first()
+        if next_page_url:
+            yield scrapy.Request(next_page_url, callback=self.parse)
 
     def parse_product(self, response):
         name = response.xpath('//div/h2/text()').extract_first()
@@ -19,11 +25,31 @@ class SpiderSpider(scrapy.Spider):
             '//p[contains(@id, "product_reference")]/span[@class="editable undertitle"]/text()').extract_first()
         price = response.xpath(
             '//div[contains(@class, "extra-product clearfix")]/span/@content').extract_first()
+        price_no_discount = response.xpath(
+            '//section[contains(@class, "regular-price")]/span/@content').extract_first()
+        price_after_discount = response.xpath(
+            '//section[contains(@class, "regular-price")]/span/@content').extract_first()
+        photo_url = response.xpath(
+            '//section[contains(@id, "content")]/a/@href').extract_first()
+        size = response.xpath(
+            '//select/option/@title').extract()
+        full_description = response.xpath(
+            '//div[contains(@class, "product-description")]/p/span/text()')
+        description = full_description.extract()[0] 
+        material = full_description.extract()[1] 
+        kolor = full_description.extract()[2].replace('color:', '')
         
         yield {
             'Nazwa': name,
             'Cena': price,
-            'Nr referencyjny': product_reference,            
+            'Nr referencyjny': product_reference,          
+            'Adres zdjecia' : photo_url, 
+            'Rozmiary' : size,
+            'Cena przed obnizka' : price_no_discount,
+            'Cena po obnizce' : price_after_discount,
+            'Opis' : description,
+            'Material wykonania' : material,
+            'Kolor' : kolor,
         }
 
 
