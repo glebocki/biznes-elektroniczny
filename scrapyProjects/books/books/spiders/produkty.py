b2ce810194f5bb18854a1517bea653715fb765aa
root <root@localhost.localdomain>
2020-12-11 17:45:34 +0400

    csv z kolumnami o nazwach z  prestashop

--- scrapyProjects/books/books/spiders/produkty.py
+++ scrapyProjects/books/books/spiders/produkty.py
@@ -1,6 +1,6 @@
 import scrapy
 category_name = ''
-sucategory_name =''
+
 
 class SpiderSpider(scrapy.Spider):
     name = 'produkty'
@@ -105,19 +105,60 @@ class SpiderSpider(scrapy.Spider):
                 if kolor.find('kolor') == 0:                
                     kolor = kolor.replace('kolor:', '')           
         material= material.capitalize()
-        
+        obnizka = 0
+        price_no_discount=price_no_discount.replace('\xa0','')
+        price_no_discount=price_no_discount.replace(',','.')
+        price=price.replace('\xa0','') 
+        if price_no_discount !='':        
+            
+            obnizka = float(price_no_discount) - float(price)
+        if obnizka == 0:
+            obnizka = ''
+        wyprzedaz = 0
+        wyprzedaz_od =''
+        wyprzedaz_do =''
+        if obnizka != '':
+            wyprzedaz = 1
+            wyprzedaz_od = '2020-11-01'
+            wyprzedaz_do = '2021-06-01'
+        producent = 'Bałdowski'
+        utworzono = '2020-09-01'
+        if categor_name == 'NOWOSCI':
+             utworzono = '2020-12-11'
+        magazyn ='Glówny magazyn'
+
         #PREPARING FOR CSV
         yield {
-            'kategoria': categor_name,              
+            'Kategoria': categor_name,              
             'Nazwa': name,
-            'Cena': price,
-            'Cena przed obnizka' : price_no_discount,
-            'Nr referencyjny': product_reference,          
-            'Adres zdjecia' : photo_url, 
+            'Cena obecna': price,
+            'Obnizka' : obnizka,
+            '#Indeks': product_reference,          
+            'Obraz' : photo_url, 
             'Rozmiary' : size,
             'Opis' : description,
-            'Material wykonania' : material,
-            'Kolor' : kolor,            
+            'Podsumowanie' : material,
+            'Kolor' : kolor,
+            'Aktywny': 1,
+            'Regula podatkowa': 1,
+            'Cana regularna przed obnizka': price_no_discount,
+            'Wyprzedaz': wyprzedaz,   
+            'Wyprzedaz od': wyprzedaz_od,
+            'Wyprzedaz do': wyprzedaz_do,
+            'Producent':  producent,
+            'Ilosc': 50,
+            'Minimalna ilosc': 1,
+            'Mala ilosc w magazynie': 4,
+            'Powiadom gdy w magazynie mało': 1,
+            'Utworzono' : utworzono,  
+            'Magazyn': magazyn,
+            'Dostepne': 1,
+            'Pokaż cene': 1,
+            'Tekst alternatywny': name,
+            'Tekst gdy na stanie': "Dostępne",
+            'Pokaz cene': 1,
+            
+
         }
         
        
